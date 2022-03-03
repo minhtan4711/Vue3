43ceaa4d74e038b23df0e832d8f24fb078a4c7ef
UongMinhTan <minhtan471100@gmail.com>
2022-03-03 16:10:24 +0700

    prop and emit event

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,26 @@
   <h1>{{ title }}</h1>
   <input type="text" ref="input_name" />
   <button @click="handleClick">Click me</button>
+  <div v-if="showModal">
+    <modal :header="header" :text="text" @close="toggleModal" />
+  </div>
+  <button @click.shift="toggleModal">Open Modal (shift)</button>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
+
 export default {
   name: "App",
+  components: {
+    Modal,
+  },
   data() {
     return {
       title: "My first vue app 2",
+      header: "some random text",
+      text: "another random text",
+      showModal: false,
     };
   },
   methods: {
@@ -18,11 +30,14 @@ export default {
       this.$refs.input_name.classList.add("active");
       this.$refs.input_name.focus();
     },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
   },
 };
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
